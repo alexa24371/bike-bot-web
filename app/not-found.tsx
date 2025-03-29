9a7d65eaef3d66e1181c288f50d89bdf94e763c5
alexa24371 <aua3656@gmail.com>
2025-03-29 19:49:02 +0000

    Not Found Update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-6xl leading-9 font-extrabold tracking-tight text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14 dark:text-gray-100">
-          404
+          404 Not Found
         </h1>
       </div>
       <div className="max-w-md">
