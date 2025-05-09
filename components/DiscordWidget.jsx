1ab4753462bff0501e984cd7498aa68f280effbd
alexa24371 <168982263+alexa24371@users.noreply.github.com>
2025-05-09 20:34:07 +0100

    Create DiscordWidget.jsx

---
+++ components/DiscordWidget.jsx
@@ -0,0 +1,13 @@
+const DiscordWidget = () => (
+  <iframe
+    src="https://discord.com/widget?id=1188468213638500453&theme=dark"
+    width="350"
+    height="500"
+    allowTransparency="true"
+    frameBorder="0"
+    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+    style={{ border: 'none' }}
+  ></iframe>
+);
+
+export default DiscordWidget;
